c8aa13dc276d6c940eea6d2775c7ee8aab41b288
nitish kumar <nitishsainin9627@gmail.com>
2024-03-04 18:58:11 +0530

    navbar changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
-/* eslint-disable no-unused-vars */
+// App.js
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+
 
 const App = () => {
-  return <div className="bg-black w-3 h-2">App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" component={<Home />} />
+          <Route path="/about" component={<About />} />
+        </Routes>
+      </BrowserRouter>
+      
+    </>
+  );
 };
 
 export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+// Navbar.js
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { IoMdMenu } from "react-icons/io";
+import { RxCross2 } from "react-icons/rx";
+const Navbar = () => {
+  const [Mobile, setMobile] = useState(true);
+  return (
+    <nav className="bg-gray-800 p-4 max-md:p-7">
+      <div className="container mx-auto flex justify-between items-center ">
+        <Link to="/" className="text-white text-2xl font-bold max-md:hidden">
+          Ticketless
+        </Link>
+        <ul
+          className={
+            Mobile ? " max-md:hidden" : "flex flex-col justify-start "
+          } onClick={()=>setMobile(false)}
+        >
+          <Link to="/" className="text-white mr-4">
+            Home
+            
+          </Link>
+          
+          <Link to="/booking" className="text-white mr-4">
+            Booking
+          </Link>
+          <Link to="/cancellation" className="text-white mr-4">
+            Cancellation
+          </Link>
+          <Link to="/about" className="text-white mr-4">
+            About us
+          </Link>
+          <Link to="/contact" className="text-white mr-4">
+            Contact us
+          </Link>
+          <Link to="/login" className="text-white mr-4">
+            Login/Signup
+          </Link>
+        </ul>
+        <button
+          className="md:hidden bg-slate-50 fixed top-5 right-5"
+          onClick={() => setMobile(!Mobile)}
+        >
+          {Mobile ? <IoMdMenu /> : <RxCross2 />}
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
